dede5ae2cd937d1d9927646a6e3652293dff9480
YatiZ <zuzu62113@gmail.com>
2024-10-08 18:58:22 +0630

    add-motion-on-btn

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,10 +1,22 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { CustomBtn } from ".";
-import { motion } from "framer-motion";
+import { color, motion } from "framer-motion";
 import Link from "next/link";
 
+const buttonVariants ={
+  visible:{
+
+  },
+  hover:{
+    scale: [1,1.1,1,1.1,1,1.1,1,1.1,1],
+    textShadow:'0px 0px 1px rgb(0,0,0)',
+    boxShadow:'0px 0px 8px rgb(0,0,0)',
+    transition:{
+      delay: 0.3
+    }
+  }
+}
 const Hero = () => {
   const handleScroll = () => {};
   return (
@@ -13,10 +25,11 @@ const Hero = () => {
         <h1  className="hero__title">Let's Chill in Our Houses</h1>
         <p className="hero__subtitle">Are you looking for renting luxury cars?</p>
 
-
-        <motion.button whileHover={{scale:1.1, boxShadow:'0px 0px 8px rgb(255,255,255)',textShadow:'0px 0px 8px rgb(255,255,255)'}} className="text-black rounded-full p-3 mt-10 border border-blue-500">
-          <Link href='/Features'>Explore Best Cars</Link>
+        <Link href='/Features'>
+        <motion.button variants={buttonVariants} whileHover='hover' className="text-black rounded-full p-3 mt-10  border-2 border-blue-500">
+          Explore Best Cars
         </motion.button>
+        </Link>
       </motion.div>
 
       <div className="hero__image-container">
