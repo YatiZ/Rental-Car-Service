2d3b251962d7c868f1429498eafaaa88385c18d7
YatiZ <zuzu62113@gmail.com>
2024-10-14 23:36:58 +0630

    update-admin

--- backend/rentals/admin.py
+++ backend/rentals/admin.py
@@ -5,12 +5,6 @@ from .models import Homes,Image, Car, CarGallery
 admin.site.register(Homes)
 admin.site.register(Image)
 
-# class HomeAdmin(admin.ModelAdmin):
-#     list_display = ('Home Names')
-
-#     @admin.display(description='Home Names')
-#     def display_home_name(self,obj):
-#         return obj.title
 
 admin.site.register(Car)
 admin.site.register(CarGallery)
