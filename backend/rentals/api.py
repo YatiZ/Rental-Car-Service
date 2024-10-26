417e3f6e96fbb4a06472571dbe80fffc6dfb4816
YatiZ <zuzu62113@gmail.com>
2024-10-26 23:52:59 +0630

    create-api-for-contact-form

--- backend/rentals/api.py
+++ backend/rentals/api.py
@@ -1,4 +1,4 @@
-from . models import Car
+from . models import Car, Contact
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from .serializers import CarListSerializer
@@ -16,3 +16,17 @@ def car_detail(request, id):
     car = Car.objects.get(id=id)
     serializer = CarListSerializer(car)
     return JsonResponse(serializer.data)
+
+@api_view(['POST'])
+def contact_form(request, id):
+    try:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact_form = Contact.objects.create(username, email, message)
+        return JsonResponse(contact_form)
+    except Exception as e:
+        print(e)
+
+        return JsonResponse({'success':False})
--- backend/rentals/urls.py
+++ backend/rentals/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('api/cars', api.car_list, name='api_car_list'),
     path('api/carList',CarListView.as_view(), name='car-list'),
     path('api/cars/<uuid:id>/',api.car_detail, name='api_car-detail'),
+    path('api/contact',api.contact_form, name='contact-form'),
 ]
